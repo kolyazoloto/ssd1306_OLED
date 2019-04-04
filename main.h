13cb817ac9c958f4ce2cfb669f8924d9af2cf5de
Kolya <kolyazoloto@mail.ru>
2019-04-04 21:15:20 +0800

    first commit

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#define RES PB1
+#define DC PB4
+#define CS PB2
+#define SCL PB5
+#define F_CPU 8000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/pgmspace.h>
+#include "font.h"
+#include "lcd1602.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif /* MAIN_H_ */
